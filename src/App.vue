c20942008d633eff776e7003305ebe2cf1e5d8fe
HOVO1997 <hsanosyan@bk.ru>
2021-02-18 14:40:47 +0400

    authorization

--- src/App.vue
+++ src/App.vue
@@ -1,12 +1,12 @@
 <template>
   <div>
-    <Header></Header>
-    <Items_of_this_season></Items_of_this_season>
-    <Home_content></Home_content>
-    <Home_content_items></Home_content_items>
-    <Expected_premiere></Expected_premiere>
-    <Partner_section></Partner_section>
-    <Footer></Footer>
+        <Header></Header>
+        <Items_of_this_season></Items_of_this_season>
+        <Home_content></Home_content>
+        <Home_content_items></Home_content_items>
+        <Expected_premiere></Expected_premiere>
+        <Partner_section></Partner_section>
+        <Footer></Footer>
   </div>
 </template>
 
@@ -19,11 +19,18 @@ import Home_content_items from "@/components/Home_content_items";
 import Expected_premiere from "@/components/Expected_premiere";
 import Partner_section from "@/components/Partner_section";
 
+
 export default {
   name: 'App',
   components: {
     Partner_section,
-    Expected_premiere, Home_content_items, Home_content, Items_of_this_season, Footer, Header},
+    Expected_premiere,
+    Home_content_items,
+    Home_content,
+    Items_of_this_season,
+    Footer,
+    Header
+  },
 }
 </script>
 
@@ -35,17 +42,18 @@ export default {
 a {
   text-decoration: none;
 }
+
 body::-webkit-scrollbar {
-  width: 12px;               /* width of the entire scrollbar */
+  width: 12px; /* width of the entire scrollbar */
 }
 
 body::-webkit-scrollbar-track {
-  background: #2A2A30;        /* color of the tracking area */
+  background: #2A2A30; /* color of the tracking area */
 }
 
 body::-webkit-scrollbar-thumb {
-  background-color: #FF5682;    /* color of the scroll thumb */
-  border-radius: 20px;       /* roundness of the scroll thumb */
-  border: 3px solid #FF5682;  /* creates padding around scroll thumb */
+  background-color: #FF5682; /* color of the scroll thumb */
+  border-radius: 20px; /* roundness of the scroll thumb */
+  border: 3px solid #FF5682; /* creates padding around scroll thumb */
 }
 </style>
---
+++ src/components/Login.vue
@@ -0,0 +1,55 @@
+<template>
+  <div class="sign section--bg" :data-bg="image" style="background: url(../src/assets/img/section/section.jpg) center center / cover no-repeat;">
+    <div class="container">
+      <div class="row">
+        <div class="col-12">
+          <div class="sign__content">
+            <!-- authorization form -->
+            <form action="#" class="sign__form">
+              <a href="index.html" class="sign__logo">
+                <img src="../assets/img/logo.svg" alt="">
+              </a>
+
+              <div class="sign__group">
+                <input type="text" class="sign__input" placeholder="Email">
+              </div>
+
+              <div class="sign__group">
+                <input type="password" class="sign__input" placeholder="Password">
+              </div>
+
+              <div class="sign__group sign__group--checkbox">
+                <input id="remember" name="remember" type="checkbox">
+                <label for="remember">Remember Me</label>
+              </div>
+
+              <button class="sign__btn" type="button">Sign in</button>
+
+              <span class="sign__text">Don't have an account? <a href="signup.html">Sign up!</a></span>
+
+              <span class="sign__text"><a href="#">Forgot password?</a></span>
+            </form>
+            <!-- end authorization form -->
+          </div>
+        </div>
+      </div>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+name: "Login",
+  data(){
+    return{
+      image: require('@/assets/img/section/section.jpg'),
+    }
+  }
+}
+</script>
+
+<style scoped>
+.sign__group--checkbox input:not(:checked) + label:after, .sign__group--checkbox input:checked + label:after {
+  content: '✓';
+}
+</style>
